22756a2ffaadc2462fbd2f1263ab41bc8b5bd501
pvqhTrick <quochung2054@gmail.com>
2023-04-19 17:48:31 +0700

    hoan thien bo va dao, them frame Nhan Vien

--- src/dao/NhanVienDao.java
+++ src/dao/NhanVienDao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import bean.NhanVien;
@@ -10,7 +11,7 @@ public class NhanVienDao {
 	ArrayList<NhanVien> dsnv=new ArrayList<NhanVien>();
 	
 	
-	public ArrayList<NhanVien> getnv(){
+	public ArrayList<NhanVien> getnv() throws SQLException{
 		ArrayList<NhanVien> dsnv = new ArrayList<NhanVien>();
 		
 		String sql="select * from NhanVien";
@@ -19,12 +20,12 @@ public class NhanVienDao {
 		
 		ResultSet rs= cmd.executeQuery();
 			while(rs.next()) {
-				int MaNhanVien = rs.getInt("MaNhanVien");
-				String HoTen = rs.getString("TenNhanVien");
-				int Tuoi = rs.getInt("Tuoi");
-				String SoDienThoai = rs.getString("SoDienThoai");
-				String DiaChi = rs.getString("DiaChi");
-				Boolean GioiTinh = rs.getBoolean("GioiTinh");
+				int MaNhanVien = rs.getInt(1);
+				String HoTen = rs.getString(2);
+				int Tuoi = rs.getInt(3);
+				String SoDienThoai = rs.getString(4);
+				String DiaChi = rs.getString(5);
+				Boolean GioiTinh = rs.getBoolean(6);
 				
 				NhanVien nhanVien = new NhanVien(MaNhanVien,HoTen,Tuoi,SoDienThoai,DiaChi,GioiTinh);
 				dsnv.add(nhanVien);
@@ -34,8 +35,8 @@ public class NhanVienDao {
 		return dsnv;
 	}
 
-	public int Them(int MaNhanVien, String HoTen,int Tuoi,String SoDienThoai,String DiaChi,Boolean GioiTinh))throws Exception{
-		String sql="";
+	public int Them(int MaNhanVien, String HoTen,int Tuoi,String SoDienThoai,String DiaChi,Boolean GioiTinh)throws Exception{
+		String sql="insert NhanVien(MaNhanVien,HoTen,Tuoi,SoDienThoai,DiaChi,GioiTinh";
 		PreparedStatement cmd= CoSo.cn.prepareStatement(sql);
 		cmd.setInt(1, MaNhanVien);
 		cmd.setString(2, HoTen);
@@ -43,6 +44,23 @@ public class NhanVienDao {
 		cmd.setString(4,SoDienThoai);
 		cmd.setString(5,DiaChi);
 		cmd.setBoolean(6,GioiTinh);
+		return cmd.executeUpdate();
+	}
+	public int Sua (int maNhanVien, String hoTen,int tuoi,String soDienThoai,String diaChi,Boolean gioiTinh)throws Exception{
+		String sql = "Update NhanVien Set MaNhanVien=maNhanVien, HoTen=hoTen, Tuoi=tuoi, SoDienThoai=soDienThoai,DiaChi=diaChi,GioiTinh=gioiTinh";
+		PreparedStatement cmd= CoSo.cn.prepareStatement(sql);
+		cmd.setInt(1, maNhanVien);
+		cmd.setString(2, hoTen);
+		cmd.setInt(3,tuoi);
+		cmd.setString(4,soDienThoai);
+		cmd.setString(5,diaChi);
+		cmd.setBoolean(6,gioiTinh);
+		return cmd.executeUpdate();
+	}
+	public int Xoa (int maNhanVien) throws Exception{
+		String sql = "delete from NhanVien where MaNhanVien = maNhanVien";
+		PreparedStatement cmd= CoSo.cn.prepareStatement(sql);
+		cmd.setInt(1,maNhanVien);
 		return cmd.executeUpdate();
 	}
 }
